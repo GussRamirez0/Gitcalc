cf8204e828b190e4dac157f2103148dda7177145
GussRamirez0 <lramirezb10@miumg.edu.gt>
2025-08-01 12:50:37 -0600

    Versión 3 - Saludo con nombre

--- program.py
+++ program.py
@@ -1,5 +1,10 @@
 # Versión 1 - Hola mundo
 print("Versión 1: ¡Hola mundo!")
+
 # Versión 2 - Suma de dos números
 print("Versión 2: Suma de 5 + 3")
 print("Resultado:", 5 + 3)
+
+# Versión 3 - Saludo personalizado
+nombre = input("¿Cómo te llamas? ")
+print(f"Hola, {nombre} 👋")
